30f4a83c35e1077cf8b1ac81e01cdfe7012225b0
iperez7928 <ismaelperezchevez@gmail.com>
2026-01-30 20:22:50 -0500

    manejo ssl con postgres

--- backendJCPD/src/config/database.ts
+++ backendJCPD/src/config/database.ts
@@ -6,10 +6,17 @@ import 'dotenv/config';
 const sequelize = new Sequelize(
   process.env.DB_NAME as string,
   process.env.DB_USER as string,
-  process.env.DB_PASSWORD ,
+  process.env.DB_PASSWORD,
+   
   {
     host: process.env.DB_HOST,
-    dialect: 'postgres', 
+    dialect: 'mysql',
+    dialectOptions: {
+				ssl: {
+					require: true,
+					rejectUnauthorized: false
+				}
+			},
     logging: false,
   }
 );
