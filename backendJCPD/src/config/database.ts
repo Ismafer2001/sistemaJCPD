fbddf083004a54100918309941dd8c517a434305
iperez7928 <ismaelperezchevez@gmail.com>
2026-01-30 20:59:37 -0500

    ARREGLO DE DBHOST

--- backendJCPD/src/config/database.ts
+++ backendJCPD/src/config/database.ts
@@ -4,7 +4,7 @@ import 'dotenv/config';
 
 
 const sequelize = new Sequelize(
-  process.env.DB_NAME as string,
+  process.env.DB_DATABASE as string,
   process.env.DB_USER as string,
   process.env.DB_PASSWORD,
    
